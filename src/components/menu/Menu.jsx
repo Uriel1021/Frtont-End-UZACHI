9a2dbd187c7a17c46361317205dfc4bb888567be
uriel1021 <aguilaruriel368@gmail.com>
2024-07-18 00:50:58 -0400

    se subio el menu corregido y responsivo

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,8 +1,6 @@
 import React, { useState } from 'react';
 import { FaTint, FaUserFriends, FaHome, FaQuestionCircle, FaEdit, FaBell, FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
 
 const Menu = () => {
   const navigate = useNavigate();
@@ -17,21 +15,6 @@ const Menu = () => {
   ];
 
   const reminderItems = [
-    { icon: <FaTint className="text-blue-500" />, type: 'Monitoreo de Agua', community: 'Santiago Comaltepec', date: '12 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-blue-100' },
-    { icon: <FaUserFriends className="text-green-500" />, type: 'Monitoreo de Suelo', community: 'Santiago Xiacui', date: '13 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-green-100' },
-    { icon: <FaHome className="text-orange-500" />, type: 'Monitoreo de Fauna', community: 'Capulalpam de Mendez', date: '14 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-orange-100' },
-    { icon: <FaTint className="text-blue-500" />, type: 'Monitoreo de Agua', community: 'Santiago Comaltepec', date: '12 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-blue-100' },
-    { icon: <FaUserFriends className="text-green-500" />, type: 'Monitoreo de Suelo', community: 'Santiago Xiacui', date: '13 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-green-100' },
-    { icon: <FaHome className="text-orange-500" />, type: 'Monitoreo de Fauna', community: 'Capulalpam de Mendez', date: '14 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-orange-100' },
-    { icon: <FaTint className="text-blue-500" />, type: 'Monitoreo de Agua', community: 'Santiago Comaltepec', date: '12 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-blue-100' },
-    { icon: <FaUserFriends className="text-green-500" />, type: 'Monitoreo de Suelo', community: 'Santiago Xiacui', date: '13 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-green-100' },
-    { icon: <FaHome className="text-orange-500" />, type: 'Monitoreo de Fauna', community: 'Capulalpam de Mendez', date: '14 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-orange-100' },
-    { icon: <FaTint className="text-blue-500" />, type: 'Monitoreo de Agua', community: 'Santiago Comaltepec', date: '12 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-blue-100' },
-    { icon: <FaUserFriends className="text-green-500" />, type: 'Monitoreo de Suelo', community: 'Santiago Xiacui', date: '13 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-green-100' },
-    { icon: <FaHome className="text-orange-500" />, type: 'Monitoreo de Fauna', community: 'Capulalpam de Mendez', date: '14 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-orange-100' },
-    { icon: <FaTint className="text-blue-500" />, type: 'Monitoreo de Agua', community: 'Santiago Comaltepec', date: '12 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-blue-100' },
-    { icon: <FaUserFriends className="text-green-500" />, type: 'Monitoreo de Suelo', community: 'Santiago Xiacui', date: '13 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-green-100' },
-    { icon: <FaHome className="text-orange-500" />, type: 'Monitoreo de Fauna', community: 'Capulalpam de Mendez', date: '14 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-orange-100' },
     { icon: <FaTint className="text-blue-500" />, type: 'Monitoreo de Agua', community: 'Santiago Comaltepec', date: '12 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-blue-100' },
     { icon: <FaUserFriends className="text-green-500" />, type: 'Monitoreo de Suelo', community: 'Santiago Xiacui', date: '13 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-green-100' },
     { icon: <FaHome className="text-orange-500" />, type: 'Monitoreo de Fauna', community: 'Capulalpam de Mendez', date: '14 de febrero del 2025', action: <FaEdit />, bgColor: 'bg-orange-100' },
@@ -64,7 +47,11 @@ const Menu = () => {
   return (
     <div className="flex flex-col md:flex-row h-screen w-11/12 mx-auto">
       {/* Menú lateral como tabla */}
-      <div className="w-full md:w-1/4 bg-white-100 p-8 mb-4 md:mb-0 md:-ml-12">
+      <div className="w-full md:w-2/4 bg-white-100 p-8 mb-4 md:mb-0 md:-ml-12">
+        <div className="mb-4">
+          <h2 className="text-xl font-bold">Bienvenido</h2>
+        </div>
+
         <div className="space-y-4">
           {menuItems.map((item, index) => (
             <div
@@ -77,7 +64,7 @@ const Menu = () => {
               </div>
               <div>
                 <h3 className="font-bold">{item.title}</h3>
-                <p>{item.description}</p>
+                <p className="hidden sm:block">{item.description}</p>
               </div>
             </div>
           ))}
@@ -85,7 +72,7 @@ const Menu = () => {
       </div>
 
       {/* Sección de Recordatorio */}
-      <div className="w-full md:w-3/4 p-4 flex flex-col">
+      <div className="w-full md:w-7/8 p-6 flex flex-col">
         <div className="mb-4">
           <h2 className="text-xl font-bold">Recordatorio</h2>
           <div className="flex items-center justify-start">
@@ -93,56 +80,60 @@ const Menu = () => {
             <span>HOY, {currentDate}</span>
           </div>
         </div>
-        <div className="flex flex-col md:flex-row flex-1">
-          
-
-          {/* Tabla */}
-          <div className="w-full md:w-4/5 ml-0 md:ml-4 bg-gray-100 p-4 h-full rounded shadow flex flex-col justify-between">
-            <table className="w-full flex-1 overflow-y-auto">
-              <tbody>
-                {currentItems.map((item, index) => (
-                  <tr key={index} className={`border-b ${item.bgColor}`}>
-                    <td className="p-2 flex flex-col md:flex-row items-start md:items-center justify-between rounded-2xl border-2">
-                      <div className="flex items-center">
-                        <div className="mr-2">{item.icon}</div>
-                        <div>
-                          <p>{item.type}</p>
-                          <p className="text-gray-600">{item.community}</p>
-                        </div>
-                      </div>
-                      <span>{item.date}</span>
-                      <button className={`mt-2 md:mt-0 ml-0 md:ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition`}>
-                        {item.action}
-                      </button>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <div className="flex justify-between mt-4">
-              <button
-                onClick={handlePrevPage}
-                disabled={currentPage === 0}
-                className={`p-2 bg-gray-500 text-white rounded hover:bg-gray-600 transition ${currentPage === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
-              >
-                <FaArrowLeft className="mr-2" /> Anterior
-              </button>
-              <button
-                onClick={handleNextPage}
-                disabled={indexOfLastItem >= reminderItems.length}
-                className={`p-2 bg-gray-500 text-white rounded hover:bg-gray-600 transition ${indexOfLastItem >= reminderItems.length ? 'opacity-50 cursor-not-allowed' : ''}`}
-              >
-                Siguiente <FaArrowRight className="ml-2" />
-              </button>
+        <div className="flex flex-col md:flex-col flex-1">
+          {/* Botón de agregar recordatorio */}
+          <div className="w-full mb-4 md:mb-0">
+            <div className="mt-4 flex justify-start">
+              <button className="w-60 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">Agregar Recordatorio</button>
             </div>
           </div>
-          {/* Calendario */}
-          <div className="w-full md:w-1/5 mb-4 md:mb-0" style={{ marginLeft: '40px' }}>
-            <div className="mt-4 mb-10">
-              <button className="block w-full p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">Agregar Recordatorio</button>
+
+          {/* Tabla y footer */}
+          <div className="w-full md:w-full bg-gray-100 p-4 flex-1 rounded shadow mt-4 relative overflow-y-auto hidden md:flex flex-col justify-between">
+            <div>
+              <table className="w-full">
+                <tbody>
+                  {currentItems.map((item, index) => (
+                    <tr key={index} className={`border-b ${item.bgColor}`}>
+                      <td className="p-2 flex items-center justify-between rounded-2xl border-2" style={{ width: '100%' }}>
+                        <div className="flex items-center">
+                          <div className="mr-2">{item.icon}</div>
+                          <div>
+                            <p>{item.type}</p>
+                            <p className="text-gray-600">{item.community}</p>
+                          </div>
+                        </div>
+                        <span>{item.date}</span>
+                        <button className="ml-4 p-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
+                          {item.action}
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-            <div style={{ height: '50%' }}>
-              <Calendar />
+            {/* Footer fijo de la tabla con botones de paginación */}
+            <div className="w-full bg-gray-100 p-4 flex justify-between items-center sticky bottom-0">
+              <span>
+                Mostrando {currentItems.length} de {reminderItems.length} elementos
+              </span>
+              <div className="flex space-x-2">
+                <button
+                  onClick={handlePrevPage}
+                  disabled={currentPage === 0}
+                  className={`flex items-center px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 transition ${currentPage === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
+                >
+                  <FaArrowLeft className="mr-2" /> Anterior
+                </button>
+                <button
+                  onClick={handleNextPage}
+                  disabled={indexOfLastItem >= reminderItems.length}
+                  className={`flex items-center px-4 py-2 bg-gray-500 text-white rounded hover:bg-gray-600 transition ${indexOfLastItem >= reminderItems.length ? 'opacity-50 cursor-not-allowed' : ''}`}
+                >
+                  Siguiente <FaArrowRight className="ml-2" />
+                </button>
+              </div>
             </div>
           </div>
         </div>
