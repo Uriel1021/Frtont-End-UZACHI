753d1324ab39cf530d1c669c8fb1ff60fd81f7ea
UrielAguilar10 <aguilaruriel368@gmail.com>
2024-10-05 22:13:22 -0400

    se realizo correcciones en el login

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,26 +1,108 @@
 import React from 'react';
+import * as Yup from 'yup';
 import { useNavigate } from 'react-router-dom';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { TextField, Box, Typography, Checkbox, FormControlLabel, Button } from '@mui/material';
+
+// Estado inicial del formulario
+const initialValues = {
+  usuario: 'u.aguilar',
+  contrasena: 'Aguilar1021',
+  recordar: false,
+};
+
+// Esquema de validación con Yup
+const validationSchema = Yup.object().shape({
+  usuario: Yup.string().required('El usuario es requerido'),
+  contrasena: Yup.string().required('La contraseña es requerida'),
+});
 
 const Login = () => {
   const navigate = useNavigate();
 
-  const handleLogin = () => {
+  const handleSubmit = (values) => {
+    // Lógica para manejar el inicio de sesión
+    //alert(JSON.stringify(values, null, 2));
     // Redirigir
     navigate('/menu');
   };
 
   return (
-    <div className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto mt-10 p-6 sm:p-8 md:p-10 lg:p-12 bg-gray-100 shadow-lg rounded-md text-center relative sm:mt-20 lg:mt-24">
-      <img src="/images/user-logo.png" alt="Logotipo" className="mx-auto mb-4 w-20 h-20 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36" />
-      <h2 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mb-4 text-green-800">Iniciar sesión</h2>
-      <div className="mb-4">
-        <input type="text" placeholder="Usuario" className="w-full px-3 py-2 border border-green-800 rounded-md mb-4 transform scale-100 sm:scale-105 md:scale-110" />
-      </div>
-      <div className="mb-4">
-        <input type="password" placeholder="Contraseña" className="w-full px-3 py-2 border border-green-800 rounded-md mb-2 transform scale-100 sm:scale-105 md:scale-110" />
-        <a href="#" className="text-sm text-green-800 hover:underline block mt-2 text-right">Restablecer contraseña</a>
-      </div>
-      <button onClick={handleLogin} className="w-full bg-green-800 text-white py-2 rounded-md hover:bg-green-700 transform scale-100 sm:scale-105 md:scale-110">Acceder</button>
+    <div className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg mx-auto mt-10 p-6 sm:p-8 md:p-10 lg:p-12 bg-gray-100 shadow-lg rounded-md relative" style={{ top: '-100px' }}>
+      {/* Logo de usuario */}
+      <img 
+        src="/images/user-logo.png" 
+        alt="Logotipo" 
+        className="mx-auto mb-4 w-20 h-20 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36" 
+      />
+      <Typography variant="h5" gutterBottom className="text-center text-green-800 font-bold">
+        Iniciar sesión
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ handleChange, handleBlur, values, touched, errors }) => (
+          <Form>
+            <Box sx={{ mb: 2 }}>
+              <Field
+                as={TextField}
+                fullWidth
+                id="usuario"
+                name="usuario"
+                label="Usuario"
+                value={values.usuario}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={Boolean(touched.usuario && errors.usuario)}
+                helperText={touched.usuario && errors.usuario}
+                variant="outlined"
+              />
+            </Box>
+            <Box sx={{ mb: 2 }}>
+              <Field
+                as={TextField}
+                fullWidth
+                type="password"
+                id="contrasena"
+                name="contrasena"
+                label="Contraseña"
+                value={values.contrasena}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={Boolean(touched.contrasena && errors.contrasena)}
+                helperText={touched.contrasena && errors.contrasena}
+                variant="outlined"
+              />
+              <Typography variant="body2" className="text-right mt-1">
+                <a href="#" className="text-green-800 hover:underline">
+                  Restablecer contraseña
+                </a>
+              </Typography>
+            </Box>
+            <FormControlLabel
+              control={
+                <Field
+                  as={Checkbox}
+                  name="recordar"
+                  color="primary"
+                />
+              }
+              label="Recordar contraseña"
+            />
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              sx={{ mt: 2 }}
+            >
+              Acceder
+            </Button>
+          </Form>
+        )}
+      </Formik>
     </div>
   );
 };
